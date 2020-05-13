34df8b63ecef00dca029ad370c8454bc69e07722
Luiz Felipe Bonatto <luizfelipebonatto@hotmail.com>
2020-05-12 23:22:03 -0300

    Update/ Initial Back-End

---
+++ public/scripts/scripts.js
@@ -0,0 +1,26 @@
+const recipe = document.querySelectorAll('.card-receitas');
+const recipe_sections = document.querySelectorAll(".recipe__section");
+
+
+for (let i = 0; i < recipe.length; i++){
+    recipe[i].addEventListener("click", function(){
+    window.location.href = `/receitas/${i}`
+    });
+}
+
+for (let recipe_section of recipe_sections) {
+    const section = recipe_section.querySelector(".recipe__description");
+    recipe_section
+      .querySelector(".recipe__expansion")
+      .addEventListener("click", function() {
+        if (section.classList.contains("active") === true) {
+          section.classList.remove("active");
+          recipe_section.querySelector(".recipe__expansion").innerHTML =
+            "ESCONDER";
+        } else {
+          section.classList.add("active");
+          recipe_section.querySelector(".recipe__expansion").innerHTML =
+            "MOSTRAR";
+        }
+      });
+  }
---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const nunjucks = require('nunjucks')
+const receitas = require('./data')
+
+const server = express()
+
+server.set('view engine','njk')
+server.use(express.static('public'))
+
+nunjucks.configure("views", {
+express:server,
+autoescape: false,
+noCache: true,
+})
+
+server.get('/', function(req, res){
+    return res.render('home', {item: receitas})
+})
+
+server.get('/about', function(req, res){
+    return res.render('about')
+})
+
+server.get('/receitas', function(req, res){
+    return res.render('receitas', {item: receitas})
+})
+
+server.get("/receitas/:index", function(req, res){
+    const recipeIndex = req.params.index;
+
+    return res.render('recipepage', {item: receitas[recipeIndex]})
+})
+
+server.use(function(req, res) {
+    res.status(404).render("not-found");
+  });
+
+server.listen(5000, function(){
+    return console.log('Server is running')
+})
